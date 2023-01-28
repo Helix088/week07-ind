708a5483b0cd146c40de9eab7b40d74d1fa54030
Helix088 <pat20001@byui.edu>
2023-01-28 10:15:39 -0700

    test

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -2,7 +2,7 @@ const checkLoggedin = (req, res, next) => {
   if (!req.oidc.isAuthenticated()) {
     return res.status(401).send({
       error: "Not authorized to change data without login.",
-      login: "http://localhost:8080/login",
+      login: "https://week07.onrender.com/login",
     });
   }
   next();
