460c9a87776314819f866754b5c690caab5f8394
Helix088 <pat20001@byui.edu>
2023-01-27 18:22:34 -0700

    test

--- routes/poke-data.js
+++ routes/poke-data.js
@@ -1,17 +1,46 @@
+// const express = require("express");
+// const routes = express.Router();
+// const authO = require('../middleware/authorize.js')
+
+// const pokeDataControl = require("../controllers/poke-data");
+// const validation = require('../middleware/validate');
+
+// routes.get("/", pokeDataControl.getPokeData);
+
+// routes.get("/:id", pokeDataControl.getPokemon);
+
+// routes.post("/", authO.checkLoggedin, validation.savePokemon, pokeDataControl.createPokemon);
+
+// routes.put("/:id", authO.checkLoggedin, validation.savePokemon, pokeDataControl.updatePokemon);
+
+// routes.delete("/:id", authO.checkLoggedin, pokeDataControl.deletePokemon);
+
+// module.exports = routes;
+
 const express = require("express");
 const routes = express.Router();
-const authO = require('../middleware/authorize.js')
+const authO = require("../middleware/authorize");
 
 const pokeDataControl = require("../controllers/poke-data");
-const validation = require('../middleware/validate');
+const validation = require("../middleware/validate");
 
 routes.get("/", pokeDataControl.getPokeData);
 
 routes.get("/:id", pokeDataControl.getPokemon);
 
-routes.post("/", authO.checkLoggedin, validation.savePokemon, pokeDataControl.createPokemon);
+routes.post(
+  "/",
+  authO.checkLoggedin,
+  validation.savePokemon,
+  pokeDataControl.createPokemon
+);
 
-routes.put("/:id", authO.checkLoggedin, validation.savePokemon, pokeDataControl.updatePokemon);
+routes.put(
+  "/:id",
+  authO.checkLoggedin,
+  validation.savePokemon,
+  pokeDataControl.updatePokemon
+);
 
 routes.delete("/:id", authO.checkLoggedin, pokeDataControl.deletePokemon);
 
