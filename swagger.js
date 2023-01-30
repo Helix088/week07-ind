16d7b34ef1336b339ec01220f762b9e244887fd1
Helix088 <pat20001@byui.edu>
2023-01-30 15:57:51 -0700

    fixed critical bug

--- swagger.js
+++ swagger.js
@@ -12,7 +12,6 @@ const doc = {
 const outputFile = "./swagger.json";
 const endpointsFiles = ["./routes/index.js"];
 
-// generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
 
 // Run server after it gets generated
